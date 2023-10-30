e7917de7cb7e767d8c16080a05fc53bcf88a01a3
NIEOKY <56267850+NIEOKY@users.noreply.github.com>
2023-10-29 19:44:07 -0600

    Seccion07 - Excepciones, interceptores y middlewares

--- DTOs/RegisterDto.cs
+++ DTOs/RegisterDto.cs
@@ -7,6 +7,7 @@ namespace CitasAppAPI.Entities
         [Required]
         public string Username { get; set; } = "";
         [Required]
-        public string Password { get; set; } = "";
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; }
     }
 }
